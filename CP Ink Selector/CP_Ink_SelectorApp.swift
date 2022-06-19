5776d1c64e90582276beb1b02d266a9702162a8f
reganlaurell <reganlaurell@gmail.com>
2022-06-19 10:07:40 -0400

    Initial Commit

---
+++ CP Ink Selector/CP_Ink_SelectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CP_Ink_SelectorApp.swift
+//  CP Ink Selector
+//
+//  Created by Regan Laurell on 6/19/22.
+//
+
+import SwiftUI
+
+@main
+struct CP_Ink_SelectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
